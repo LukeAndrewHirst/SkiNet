a38b02ba5cc3eced3b60098f6b054fe23d2f389f
LukeAndrewHirst <luke.hirst.la@gmail.com>
2025-01-10 07:18:50 +0200

    Implemented generic repository & specification pattern

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -1,24 +1,28 @@
-using System.Diagnostics;
 using Core.Entities;
 using Core.Interfaces;
+using Core.Specifications;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
-    public class ProductsController(IProductRepository productRepository) : ControllerBase
+    public class ProductsController(IGenericRepository<Product> repository) : ControllerBase
     {
         [HttpGet]
         public async Task<ActionResult<IReadOnlyList<Product>>> GetProducts(string? brand, string? type, string? sort)
         {
-            return Ok(await productRepository.GetProductsAsync(brand, type, sort));
+            var spec = new ProductSpecification(brand, type, sort);
+
+            var products = await repository.ListAsync(spec);
+
+            return Ok(products);
         }
 
         [HttpGet("{id:int}")]
         public async Task<ActionResult<Product>> GetProduct(int id)
         {
-            var product = await productRepository.GetProductByIdAsync(id);
+            var product = await repository.GetByIdAsync(id);
             if(product == null) return NotFound();
             
             return product;
@@ -27,21 +31,25 @@ namespace API.Controllers
         [HttpGet("brands")]
         public async Task<ActionResult<IReadOnlyList<string>>> GetBrands()
         {
-            return Ok(await productRepository.GetBrandsAsync());
+            var spec = new BrandListSpecification();
+
+            return Ok(await repository.ListAsync(spec));
         }
 
         [HttpGet("types")]
         public async Task<ActionResult<IReadOnlyList<string>>> GetTypes()
         {
-            return Ok(await productRepository.GetTypesAsync());
+            var spec = new TypeListSpecification();
+
+            return Ok(await repository.ListAsync(spec));
         }
 
         [HttpPost]
         public async Task<ActionResult<Product>> AddProduct(Product product)
         {
-            productRepository.AddProduct(product);
+            repository.Add(product);
 
-            if(await productRepository.SaveChangesAsync()) return CreatedAtAction("GetProduct", new {id = product?.Id}, product);
+            if(await repository.SaveAllAsync()) return CreatedAtAction("GetProduct", new {id = product?.Id}, product);
 
             return BadRequest("Unable to create new product");
         }
@@ -51,9 +59,9 @@ namespace API.Controllers
         {
             if(product.Id != id || !ProductExisits(id)) return BadRequest("Update to update this product");
 
-            productRepository.UpdateProduct(product);
+            repository.Update(product);
 
-            if(await productRepository.SaveChangesAsync()) return NoContent();
+            if(await repository.SaveAllAsync()) return NoContent();
 
             return BadRequest("Unable to update product");
         }
@@ -61,19 +69,19 @@ namespace API.Controllers
         [HttpDelete("{id:int}")]
         public async Task<ActionResult> DeleteProduct(int id)
         {
-            var product = await productRepository.GetProductByIdAsync(id);
+            var product = await repository.GetByIdAsync(id);
             if(product == null) return NotFound();
 
-            productRepository.DeleteProduct(product);
+            repository.Remove(product);
 
-            if(await productRepository.SaveChangesAsync()) return NoContent();
+            if(await repository.SaveAllAsync()) return NoContent();
 
             return BadRequest("Unable to delete product");
         }
 
         private bool ProductExisits(int id)
         {
-            return productRepository.ProductExists(id);
+            return repository.Exists(id);
         }
     }
 }
