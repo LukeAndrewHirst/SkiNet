e7886f739d574b269dd6d1c9bf570c7e219b4764
LukeAndrewHirst <luke.hirst.la@gmail.com>
2025-01-10 23:33:37 +0200

    Implemented the header & http client to fetch data

---
+++ web-app/src/app/shared/models/product.ts
@@ -0,0 +1,10 @@
+export interface Product {
+    id: number,
+    name: string,
+    description: string,
+    type: string,
+    brand: string,
+    pictureUrl: string,
+    price: number,
+    quantityInStock: number
+}
