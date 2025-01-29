26491e6b804a62dfb1ab1e418eb47fb5e308b478
LukeAndrewHirst <luke.hirst.la@gmail.com>
2025-01-29 06:07:46 +0200

    Implemented client side authentication

---
+++ web-app/src/app/shared/models/user.ts
@@ -0,0 +1,15 @@
+export type User = {
+    firstName: string,
+    lastNamr: string,
+    email: string,
+    address: string
+}
+
+export type Address = {
+    line1: string,
+    line12: string,
+    country: string,
+    city: string,
+    state: string,
+    postalCode: string
+}
